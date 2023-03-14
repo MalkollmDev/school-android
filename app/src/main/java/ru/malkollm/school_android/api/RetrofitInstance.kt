d6d0d1f747d16d8215550484c108820f3e06bda8
Фаткуллин Марсель Саматович <FatkullinMS@HM-GES.RU>
2023-03-14 10:26:18 +0500

    Revert "Попытка обратиться к серверу за данными"
    This reverts commit 1d786a77047b5cd68ebf4b806739a52ee030a8f3.

--- app/src/main/java/ru/malkollm/school_android/api/RetrofitInstance.kt
+++ app/src/main/java/ru/malkollm/school_android/api/RetrofitInstance.kt
@@ -2,7 +2,6 @@ package ru.malkollm.school_android.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import ru.malkollm.school_android.api.interfaces.ScheduleApi
 import ru.malkollm.school_android.api.interfaces.TodoApi
 
 object RetrofitInstance {
@@ -12,13 +11,5 @@ object RetrofitInstance {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TodoApi::class.java)
-    }
-
-    val apiSchedule: ScheduleApi by lazy {
-        Retrofit.Builder()
-            .baseUrl("http://api.malkollm.ru/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ScheduleApi::class.java)
     }
 }
--- app/src/main/java/ru/malkollm/school_android/models/Lesson.kt
+++
@@ -1,6 +0,0 @@
-package ru.malkollm.school_android.models
-
-data class Lesson(
-    val id: Int,
-    val name: String
-)
